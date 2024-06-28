d42a260fae2a4a9e85c630abe9a76c4fdd132ea5
hollowww-dev <me@hollowww.dev>
2024-06-28 18:36:01 +0300

    test

--- app/components/FeedbackEntry/index.tsx
+++ app/components/FeedbackEntry/index.tsx
@@ -31,7 +31,7 @@ export const FeedbackEntrySkeleton = ({ extend }: { extend: boolean }) => {
 				<p style={{ width: "60%" }}>
 					<Skeleton />
 				</p>
-				<Skeleton width={50} height={25} />
+				<Skeleton width={50} height={29} />
 			</div>
 			<div className={clsx(`${styles.votes}`, `${styles.skeleton}`)} />
 		</div>
